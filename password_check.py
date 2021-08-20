014cd61ed06360d035ce3487560b91ea491d6259
Artrix-8 <69241278+Artrix-8@users.noreply.github.com>
2021-08-20 20:15:36 +1000

    Added password check program

---
+++ password_check.py
@@ -0,0 +1,6 @@
+MIN_PASS = 4
+password = input(f"Create Password (must be {MIN_PASS} characters long): ")
+while len(password) < MIN_PASS:
+    print("Password too short!!!")
+    password = input("Enter password of at least {} characters: ".format(MIN_PASS))
+print('*' * len(password))
